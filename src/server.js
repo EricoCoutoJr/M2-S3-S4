acc0fbeb17c0d46177dde1da2faa846aa1473eb2
Erico Couto Jr <erico.coutjr@gmail.com>
2023-05-25 18:31:33 -0300

    feat: Create a basic API configuration using Express.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
+// Estrutura da API sem funcionalidades
+
+/**
+ * Importanto  e instaciando o Express na variável app
+ */
 const express = require('express')
 const app= express()
 
+/**
+ * Definindo porta do servidor
+ */
 const port=3333
 
 // Informando a aplicação Express que será usado o formato JSON para trasferir e receber dados
@@ -12,4 +20,7 @@ app.use(express.json())
 // app.use(routes)
 
 
+/**
+ * Ativando o servidor e disponibilizando a porta definida anteriormente na variável port
+ */
 app.listen(port, ()  => console.log(`Seridor rodando na porta ${port}`))
